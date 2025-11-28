895d78ec9dd93c928081963db086b2866c1e6692
NearlyLloyd <Lloydfalltrick12@gmail.com>
2025-11-28 14:45:18 +0000

    Database implementation

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,52 @@ import Control.Applicative
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 
+data Users = Users Int String deriving (Show)
+
 data TestField = TestField Int String deriving (Show)
 
-instance FromRow TestField where
-    fromRow = TestField <$> field <*> field
+
+instance FromRow Users where
+    fromRow = Users <$> field <*> field
+
+
 
 main :: IO ()
 main = do
     conn <- open "app/Database/test.db"
     --execute conn "INSERT INTO test (str) VALUES (?)" (Only ("test string 4" :: String))
-    r <- query_ conn "SELECT * from test" :: IO [TestField]
+    execute_ conn "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, Name TEXT)"
+    --execute conn "DELETE FROM users" ()
+    --execute conn "DROP TABLE users"()
+
+    --execute conn "INSERT INTO Users (Name) VALUES (?)"(Only ("John":: String))
+    r <- query_ conn "SELECT * from Users" :: IO [Users]
     mapM_ print r
     close conn
 
 
-myUser :: Int -> String -> User
-myUser n s = User { userId = n, name = s }
+
+selectUser :: String -> IO ()
+selectUser s = do
+    conn <- open "app/Database/test.db"
+    r <- queryNamed conn "SELECT * FROM USERS WHERE Name = :Name"[":Name" := s] :: IO[Users]
+    mapM_ print r
+    close conn
+
+
+
+
+
+
+-- myUser :: Int -> String -> User
+-- myUser n s = User { userId = n, name = s }
 
 data Book = Book{
     bookId :: Int,
     title :: String,
     author :: String,
-    status :: Bool 
+    status :: Bool,
+    user_borrowed_ID :: Maybe Int
 } deriving (Show)
 
 
