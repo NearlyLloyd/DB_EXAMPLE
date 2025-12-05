e2e6fe38b178f429dfaedd407918f859e1a3ffdc
NearlyLloyd <lloydfalltrick12@gmail.com>
2025-12-05 18:36:17 +0000

    addUser and borrowBook added

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,11 @@
 import System.Exit
 import Control.Applicative
 import Database.SQLite.Simple
-import Database.SQLite.Simple.FromRow
 import Control.Monad
 
 
 data Users = Users Int String deriving (Show)
-data Books = Books Int String String String deriving (Show)
+data Books = Books Int String String String (Maybe Int) deriving (Show)
 data TestField = TestField Int String deriving (Show)
 
 
@@ -15,22 +14,19 @@ instance FromRow Users where
     fromRow = Users <$> field <*> field
 
 instance FromRow Books where
-    fromRow = Books <$> field <*> field <*> field <*> field
+    fromRow = Books <$> field <*> field <*> field <*> field <*> field
 
 
 
 main :: IO ()
 main = do
-    
     conn <- open "app/Database/test.db"
-    --execute conn "INSERT INTO test (str) VALUES (?)" (Only ("test string 4" :: String))
     execute_ conn "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, Name TEXT)"
-    execute_ conn "CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Status TEXT)"
+    execute_ conn "CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Status TEXT,BorrowedBy int)"
     --execute conn "DELETE FROM users" ()
-    --execute conn "DROP TABLE users"()
+    --execute conn "DROP TABLE Books"()
     --execute conn "INSERT INTO Users (Name) VALUES (?)"(Only ("John":: String))
     --r <- query_ conn "SELECT * from Users" :: IO [Users]
-    --mapM_ print r
     close conn
     flowHelper
 
@@ -50,12 +46,13 @@ handleCommand cmd = do
         "exit" -> exitSuccess
         ":q" -> exitSuccess
         "addBook" -> addBook
-        "listBooks" -> listBooks
+        "availableBooks" -> listBooks
+        "removeBook" -> removeBook
+        "addUser" -> addUser
+        "borrowBook" -> borrowBook
         _ -> putStrLn "ERROR: that command is not recognised, type help for a list of valid commands\n"
     flowHelper
     
-    
-
 
 selectUser :: String -> IO ()
 selectUser s = do
@@ -71,10 +68,10 @@ help = do
     putStrLn "exit - exits out of the program"
     putStrLn "addBook - adds a new book"
     putStrLn "removeBook - removes a book if it is available"
-    putStrLn "*NOT IMPLEMENTED*"
     putStrLn "addUser - adds a new user"
-    putStrLn "removeUser - removes a user if they have no borrowed books"
     putStrLn "borrowBook - marks a book as borrowed by a user"
+    putStrLn "*NOT IMPLEMENTED*"
+    putStrLn "removeUser - removes a user if they have no borrowed books"
     putStrLn "returnBook - marks a book as returned by the borrowing user"
     putStrLn "availableBooks - lists all available books"
     putStrLn "borrowedBooks - lists all borrowed books with the borrowers"
@@ -95,27 +92,51 @@ addBook = do
     putStrLn "Enter Author name: "
     author <- getLine
     conn  <- open "app/Database/test.db"
-    execute conn "INSERT INTO Books(Title,Author,Status) VALUES (?,?,?)" [title, author, "Available"]
+    execute conn "INSERT INTO Books(Title,Author,Status,BorrowedBy) VALUES (?,?,?,null)" [title, author, "Available"]
     close conn
 
 removeBook ::IO()
 removeBook = do
     putStrLn "Enter Book Title: "
-    title <- getLine
+    titleQuery <- getLine
     putStrLn "Enter Author: "
-    author <- getLine
+    authorQuery <- getLine
     conn  <- open "app/Database/test.db"
-    r <- queryNamed conn "SELECT * FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := title,":Author" := author, ":Status" := ("Available"  :: String)] :: IO[Books]
-    
-    mapM_ print r
+    r <- queryNamed conn "SELECT * FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := titleQuery,":Author" := authorQuery, ":Status" := ("Available"  :: String)] :: IO[Books]
+    _ <- queryNamed conn "DELETE FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := titleQuery,":Author" := authorQuery, ":Status" := ("Available"  :: String)] :: IO[Books]
+    forM_ r $ \row ->
+        putStrLn $ show row ++ " Deleted"
     close conn
 
 
 listBooks :: IO()
 listBooks = do
         conn <- open "app/Database/test.db"
-        r <- query_ conn "SELECT * FROM Books" :: IO[Books]
+        r <- query_ conn "SELECT * FROM Books WHERE status = 'Available'" :: IO[Books]
         mapM_ print r
+
+
+addUser :: IO()
+addUser = do
+    putStrLn "Enter the user's name"
+    name <- getLine
+    conn <- open "app/Database/test.db"
+    execute conn "INSERT INTO Users(Name) VALUES (?)" [name]
+    let outputText = "User: " ++ name ++ " added to database!"
+    putStrLn outputText 
+    close conn
+
+borrowBook ::IO()
+borrowBook = do
+    putStrLn "Enter user's ID: "
+    userId <- getLine
+    putStrLn "Enter Book ID: "
+    bookId <- getLine
+    conn <- open "app/Database/test.db"
+    execute conn "UPDATE Books SET BorrowedBy = ?, Status = 'Borrowed' WHERE id = ? AND Status = 'Available' "[userId,bookId]
+    close conn
+
+
 
 
 
