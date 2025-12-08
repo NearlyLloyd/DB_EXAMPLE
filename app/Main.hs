a6ef9650d0dc8e7e44fcc5c606a9cf0f64469df3
NearlyLloyd <lloydfalltrick12@gmail.com>
2025-12-08 21:02:46 +0000

    BorrowedBooks + availableBooks

--- app/Main.hs
+++ app/Main.hs
@@ -9,23 +9,19 @@ data Users = Users Int String deriving (Show)
 data Books = Books Int String String String (Maybe Int) deriving (Show)
 data ID = ID Int deriving(Show)
 
-data UsersBooksJoined = UsersBooksJoined Int String Int String String String (Maybe Int) deriving (Show)
-
 instance FromRow Users where
     fromRow = Users <$> field <*> field
 
 instance FromRow Books where
     fromRow = Books <$> field <*> field <*> field <*> field <*> field
 
-instance FromRow UsersBooksJoined where
-    fromRow = UsersBooksJoined <$> field <*> field <*> field <*> field <*> field <*> field <*> field
 
 
 main :: IO ()
 main = do
     conn <- open "app/Database/test.db"
     execute_ conn "CREATE TABLE IF NOT EXISTS Users (UserID INTEGER PRIMARY KEY, Name TEXT)"
-    execute_ conn "CREATE TABLE IF NOT EXISTS Books (BookID INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Status TEXT,BorrowedBy int)"
+    execute_ conn "CREATE TABLE IF NOT EXISTS Books (BookID INTEGER PRIMARY KEY, Title TEXT, Author TEXT, Status TEXT,BorrowedBy Int)"
     --execute conn "DELETE FROM users" ()
     --execute conn "DROP TABLE Books"()
     --execute conn "DROP TABLE Users"()
@@ -56,6 +52,9 @@ handleCommand cmd = do
         "addUser" -> addUser
         "removeUser" -> removeUser
         "borrowBook" -> borrowBook
+        "returnBook" -> returnBook
+        "availableBooks" -> availableBooks
+        "borrowewdBooks" -> borrowedBooks
         _ -> putStrLn "ERROR: that command is not recognised, type help for a list of valid commands\n"
     flowHelper
     
@@ -77,12 +76,12 @@ help = do
     putStrLn "addUser - adds a new user"
     putStrLn "removeUser - removes a user if they have no borrowed books"
     putStrLn "borrowBook - marks a book as borrowed by a user"
-    putStrLn "*NOT IMPLEMENTED*"
     putStrLn "returnBook - marks a book as returned by the borrowing user"
     putStrLn "availableBooks - lists all available books"
     putStrLn "borrowedBooks - lists all borrowed books with the borrowers"
-    putStrLn "listUsers - lists all users"
 
+    putStrLn "*NOT IMPLEMENTED*"
+    putStrLn "listUsers - lists all users"
 
 
 addBook :: IO ()
@@ -102,11 +101,8 @@ removeBook = do
     putStrLn "Enter Author: "
     authorQuery <- getLine
     conn  <- open "app/Database/test.db"
-    --r <- queryNamed conn "SELECT (BookID,Title,Author) FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := titleQuery,":Author" := authorQuery, ":Status" := ("Available"  :: String)] :: IO[Books]
     r <- queryNamed conn "SELECT BookID,Title,Author FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := titleQuery,":Author" := authorQuery, ":Status" := ("Available"  :: String)]
     _ <- queryNamed conn "DELETE FROM Books WHERE (Title = :Title AND Author = :Author AND Status = :Status)" [":Title" := titleQuery,":Author" := authorQuery, ":Status" := ("Available"  :: String)] :: IO[Books]
-
-
     forM_ r $ \(id,title,author) ->
         putStrLn $ Text.unpack "Title: " ++ title ++ " Written by: " ++ author ++ " REMOVED. ID: "++show (id ::Int)
     close conn
@@ -152,14 +148,13 @@ removeUser = do
 
             if null r2
                 then do 
-                    execute conn "DELETE FROM BOOKS BorrowedBy = ?" [userId]
+                    execute conn "DELETE FROM Users WHERE UserID = ?" [userId]
                     putStrLn "Deleted user successfully!" 
 
                 else putStrLn ("User is currently borrowing " ++ show(length r2) ++ " Book(s)")
             forM_ r2 $ \(id,title) ->
                 putStrLn $ Text.unpack "Title: " ++ title ++ " BookID: " ++ show (id ::Int)
-
-         
+    
     close conn
 
 returnBook :: IO ()
@@ -167,16 +162,38 @@ returnBook = do
     putStrLn "enter ID of BOOK to be returned: "
     bookId <- getLine
     conn <- open "app/Database/test.db"
-    r <- queryNamed conn "SELECT BorrowedBy,title FROM Books WHERE BookID = :bookId " [":bookId" := bookId] :: IO[Users]
+    r <- queryNamed conn "SELECT * FROM Books WHERE BookID = :bookId AND Status = 'Borrowed' " [":bookId" := bookId] :: IO[Books]
+    if null r
+        then putStrLn "Book either does not exist or has already been returned"
+        else do
+            execute conn "UPDATE Books SET BorrowedBy = null, Status = 'Borrowed' WHERE BookID = ? "[bookId]
+            putStrLn "Book successfully returned!"
+
     close conn
 
+availableBooks :: IO ()
+availableBooks = do
+    conn <- open "app/Database/test.db"
+    r <- queryNamed conn "SELECT BookID,Title FROM Books WHERE Status = 'Available' "[]
+    if null r 
+        then putStrLn "There are no available books at this moment"
+        else do
+            putStrLn "\nAvailable Books: "
+            forM_ r $ \(id,title) ->
+                putStrLn $ Text.unpack "Title: " ++ title ++ " : BookID: " ++ show (id ::Int)
 
+    close conn
+    
+borrowedBooks :: IO ()
+borrowedBooks = do
+    conn <- open "app/Database/test.db"
+    r <- queryNamed conn "SELECT BookID, Title, BorrowedBy FROM Books WHERE Status = 'Borrowed' "[]
+    if null r 
+        then putStrLn "There are no borrowed books at this moment"
+        else do
+            putStrLn "\nBorrowed Books: "
+            forM_ r $ \(id,title,userId) ->
+                --r2 <- queryNamed conn "SELECT Name FROM Users WHERE UserID = ? "[userId]
+                putStrLn $ Text.unpack "Title: " ++ title ++ " : BookID: " ++ show (id ::Int) ++ " : Borrowed by userID: " ++ show(userId :: Int)
 
-
-
-
-
-
-
-
-
+    close conn
